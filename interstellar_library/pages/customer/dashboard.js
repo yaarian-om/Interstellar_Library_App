a012cfaea555a0f93183f2d9402b23f7e94e547c
Shakib Parvez <shakibparvez75@gmail.com>
2023-08-28 02:32:24 +0600

    added profile image

--- interstellar_library/pages/customer/dashboard.js
+++ interstellar_library/pages/customer/dashboard.js
@@ -169,7 +169,7 @@ export default function Login() {
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
-                                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                                 </div>
                             </label>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
