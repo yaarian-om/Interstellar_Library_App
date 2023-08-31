fd30838026bed8d1d8708a75946b2dd8a04dd5ae
yaarian-om <dip.kumar020@gmail.com>
2023-09-01 02:00:16 +0600

    Bug fixing for Deployment

--- interstellar_library/pages/utils/globalConstantUtil.js
+++ interstellar_library/pages/utils/globalConstantUtil.js
@@ -1,11 +1,11 @@
 
-module.exports = Object.freeze({
-    MODAL_BODY_TYPES : {
-        USER_DETAIL : "USER_DETAIL",
-        DEFAULT : "",
-    },
+// module.exports = Object.freeze({
+//     MODAL_BODY_TYPES : {
+//         USER_DETAIL : "USER_DETAIL",
+//         DEFAULT : "",
+//     },
 
-    RIGHT_DRAWER_TYPES : {
-        NOTIFICATION : "NOTIFICATION",
-    }
-});
+//     RIGHT_DRAWER_TYPES : {
+//         NOTIFICATION : "NOTIFICATION",
+//     }
+// });
