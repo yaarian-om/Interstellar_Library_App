1e02c6f0664e6ff999d2c2da3b64e82cd227c6c9
yaarian-om <dip.kumar020@gmail.com>
2023-09-01 02:17:23 +0600

    Bug fixing for Deployment

--- interstellar_library/pages/components/layout/seller-layout/search_results.js
+++ interstellar_library/pages/components/layout/seller-layout/search_results.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useAuth } from "../../../../utils/authcontext"
+import { useAuth } from "../../../utils/authcontext"
 
 export default function _Search_Result_List() {
 
